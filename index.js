f8ecc7482b2525bba0069a96c96b392d28468048
JiuYan <jiuyan64@gmail.com>
2021-12-25 00:11:21 +0800

    fix: md

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const schedule = require('node-schedule')
 const req = require('child_process')
 const signTask = () => {
-    // 设置为每天23:59:59打开首页进行签到
+    // 设置为每天0:00:00打开首页进行签到
     // 时间格式为'秒 分 时 日 月 年'，* 代表每天/每月/每年
     schedule.scheduleJob('0 0 0 * * *', () => {
         let nowTime = new Date();
