2da5c773d5309a29e157b0521e9e3250741534a1
JiuYan <jiuyan64@gmail.com>
2021-12-25 00:04:04 +0800

    fix: index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const req = require('child_process')
 const signTask = () => {
     // 设置为每天23:59:59打开首页进行签到
     // 时间格式为'秒 分 时 日 月 年'，* 代表每天/每月/每年
-    schedule.scheduleJob('59 59 23 * * *', () => {
+    schedule.scheduleJob('0 0 0 * * *', () => {
         let nowTime = new Date();
         req.exec('start https://www.gtloli.gay/forum.php')
         console.log("----->" + nowTime.toLocaleDateString() + "-开始签到<-----");
