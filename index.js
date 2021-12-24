e517d36bd7d74f46ec6b60b972e1553819969151
JiuYan <jiuyan64@gmail.com>
2021-12-24 22:43:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const schedule = require('node-schedule')
+const req = require('child_process')
+const signTask = () => {
+    // 设置为每天23:59:59打开首页进行签到
+    // 时间格式为'秒 分 时 日 月 年'，* 代表每天/每月/每年
+    schedule.scheduleJob('59 59 23 * * *', () => {
+        let nowTime = new Date();
+        req.exec('start https://www.gtloli.gay/forum.php')
+        console.log("----->" + nowTime.toLocaleDateString() + "-开始签到<-----");
+    })
+}
+signTask()
